ddd3af1594190314ff06bc7cfd89d8518d75ba2a
arthur_dev <SobaNoodles@lavabit.com>
2022-09-14 01:09:59 -0700

    PLS DONT RUN THIS I WAS DRUNK CODING THIS HAHAH

---
+++ HOTHOTHOT.py
@@ -0,0 +1,34 @@
+while(True):
+  print('when this song come on in the club they gon be like')
+  damn = str(input(''))
+  if damn == 'damn thats hot':
+    print('WHOOOP')
+    break
+  else:
+    break
+  
+while(True):
+  print('and when they gon play it in the car they gon drop they tops like')
+  damn2 = str(input(''))
+  if damn2 == 'damn thats hot':
+    print('WHOOOOP')
+    break
+  else:
+    break
+
+while(True):
+  print('they gon mix it like biggie IT WAS ALLL A DREAM LIKE')
+  damn3 = str(input(''))
+  if damn3 == 'damn thats hot':
+    print('ME KIM KEYSHIA WONT STOP TIL THE TICK DONT TOK LIKE')
+    break
+  else:
+    break
+
+while(True):
+  damn4 = str(input(''))
+  if damn4 == 'damn thats hot':
+    print('yessssss')
+    break
+  else:
+    break
